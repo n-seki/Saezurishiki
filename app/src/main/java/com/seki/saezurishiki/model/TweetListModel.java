2cbe099233d39e599bd9351cb86c690c178bbf71
Naoto Uwaseki <developer.seki@gmail.com>
2017-03-19 15:07:32 +0900

    tweetListModel extends StatusUserStreamListener

--- app/src/main/java/com/seki/saezurishiki/model/TweetListModel.java
+++ app/src/main/java/com/seki/saezurishiki/model/TweetListModel.java
@@ -2,10 +2,14 @@ package com.seki.saezurishiki.model;
 
 
 import com.seki.saezurishiki.model.util.ModelObserver;
+import com.seki.saezurishiki.network.twitter.streamListener.StatusUserStreamListener;
 
 import twitter4j.Paging;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.User;
 
-public interface TweetListModel extends ModelBase {
+public interface TweetListModel extends ModelBase, StatusUserStreamListener {
 
     void request(Paging paging);
 
@@ -13,4 +17,13 @@ public interface TweetListModel extends ModelBase {
     void addObserver(ModelObserver observer);
     @Override
     void removeObserver(ModelObserver observer);
+
+    @Override
+    void onStatus(Status tweet);
+    @Override
+    void onDeletionNotice(StatusDeletionNotice deletionNotice);
+    @Override
+    void onFavorite(User sourceUser, User targetUser, Status targetTweet);
+    @Override
+    void onUnFavorite(User sourceUser, User targetUser, Status targetTweet);
 }
