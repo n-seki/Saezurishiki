c0e1c99518bab93cd1fa107aa8fbf082a6290b3e
Naoto Uwaseki <developer.seki@gmail.com>
2017-04-01 17:00:02 +0900

    add FavoriteListModel class

---
+++ app/src/main/java/com/seki/saezurishiki/model/impl/FavoriteListModel.java
@@ -0,0 +1,42 @@
+package com.seki.saezurishiki.model.impl;
+
+import com.seki.saezurishiki.entity.TweetEntity;
+import com.seki.saezurishiki.model.adapter.ModelActionType;
+import com.seki.saezurishiki.model.adapter.ModelMessage;
+import com.seki.saezurishiki.network.twitter.TwitterAccount;
+
+import java.util.List;
+
+import twitter4j.Paging;
+import twitter4j.ResponseList;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+public class FavoriteListModel extends TweetListModelImp {
+
+
+    FavoriteListModel(TwitterAccount twitterAccount) {
+        super(twitterAccount);
+    }
+
+    @Override
+    public void request(final Paging paging) {
+        final Twitter twitter = this.twitterAccount.twitter;
+        this.executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final ResponseList<Status> result = twitter.getFavorites(paging);
+                    final List<TweetEntity> tweets = twitterAccount.getRepository().map(result);
+                    twitterAccount.getRepository().add(result);
+                    final ModelMessage message = ModelMessage.of(ModelActionType.LOAD_REPLY, tweets);
+                    observable.notifyObserver(message);
+                } catch (TwitterException e) {
+                    final ModelMessage error = ModelMessage.error(e);
+                    observable.notifyObserver(error);
+                }
+            }
+        });
+    }
+}
