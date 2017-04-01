94e89dc95bae8d38d66e9638c955e728ac03276c
Naoto Uwaseki <developer.seki@gmail.com>
2017-04-02 00:05:58 +0900

    add FavoriteListPresenter

---
+++ app/src/main/java/com/seki/saezurishiki/presenter/list/FavoriteListPresenter.java
@@ -0,0 +1,31 @@
+package com.seki.saezurishiki.presenter.list;
+
+import com.seki.saezurishiki.entity.TweetEntity;
+import com.seki.saezurishiki.entity.UserEntity;
+import com.seki.saezurishiki.model.TweetListModel;
+import com.seki.saezurishiki.model.adapter.ModelMessage;
+
+import java.util.List;
+
+
+public class FavoriteListPresenter extends TweetListPresenter {
+
+
+    public FavoriteListPresenter(TweetListView view, UserEntity listOwner, TweetListModel listModel) {
+        super(view, listOwner, listModel);
+    }
+
+
+    @SuppressWarnings("unchecked")
+    @Override
+    void dispatch(ModelMessage message) {
+        switch (message.type) {
+            case LOAD_FAVORITE:
+                this.view.loadTweets((List<TweetEntity>)message.data);
+                break;
+
+            default:
+                //no operation
+        }
+    }
+}
