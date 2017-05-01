db4e7b5e72a7d52226d79a5111b924bb6ea2419a
Naoto Uwaseki <developer.seki@gmail.com>
2017-05-01 17:14:23 +0900

    fix : load already loaded tweet when load button loading

--- app/src/main/java/com/seki/saezurishiki/view/fragment/list/TweetListFragment.java
+++ app/src/main/java/com/seki/saezurishiki/view/fragment/list/TweetListFragment.java
@@ -403,7 +403,7 @@ public abstract class TweetListFragment extends Fragment
 
 
     protected void loadTimeLine() {
-        final long maxID = this.getLastId();
+        final long maxID = this.getLastId() - 1;
         this.presenter.load(new RequestInfo().maxID(maxID == -1 ? 0 : maxID).count(50));
     }
 
