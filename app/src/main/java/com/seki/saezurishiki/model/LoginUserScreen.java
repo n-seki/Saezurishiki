8a8786ddf131ef93457fa6599c398d5edfe262f7
n-seki <developer.n.seki@gmail.com>
2018-01-07 16:22:38 +0900

    remove unused import

--- app/src/main/java/com/seki/saezurishiki/model/LoginUserScreen.java
+++ app/src/main/java/com/seki/saezurishiki/model/LoginUserScreen.java
@@ -2,8 +2,6 @@ package com.seki.saezurishiki.model;
 
 import com.seki.saezurishiki.network.twitter.streamListener.CustomUserStreamListener;
 
-import twitter4j.StatusUpdate;
-
 public interface LoginUserScreen extends ModelBase, CustomUserStreamListener {
     void getLoginUser();
     void startUserStream();
